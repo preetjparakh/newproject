5a583e04ff6fe83f5385c45c515f1b8cdf6b4585
preetjparakh <2x2za4@gmail.com>
2026-08-14 22:47:41 +0530

    subtract

--- utils.js
+++ utils.js
@@ -8,3 +8,14 @@ function add(a, b) {
 
 console.log(greet("Developer"));
 console.log("Sum:", add(10, 20));
+
+function now(name) {
+    return `Heyy, ${name}!`;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+console.log(now("Developer"));
+console.log("Difference:", minus(100, 20));
